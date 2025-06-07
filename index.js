16cc465e9215a149c5712c3e53f8b790c9f5b4ca
Code Fiesta <17772633+codefiesta@users.noreply.github.com>
2025-06-06 21:34:29 -0700

    Xcode Averages (#4)

--- index.js
+++ index.js
@@ -19,16 +19,44 @@ class LLVMCoverageReader {
             const data = require(file);
             console.log(data);
 
-            // Read the code coverage percentages for every package file
-            const testedFiles = data['data'][0]['files'];
-            const testFileCount = testedFiles.length;
-            var accumlative = 0;
-            for (const i in testedFiles) {
-                accumlative += testedFiles[i]['summary']['lines']['percent'];
-            }
+            // Create 2 groups of files (Source + Source&Test)
+            const testAndSourceFiles = data['data'][0]['files'].filter((file) => !file.filename.includes('.build'));
+            const sourceFiles = testAndSourceFiles.filter((file) => !file.filename.includes('Tests'));
             
-            let percentage = Math.floor(((accumlative / (100 * testFileCount)) * 100)).toFixed(0);
-            console.log(`Coverage Percentage [${percentage}]`);
+            // Iterate through the source files
+            var sourceLineCount = 0;
+            var sourceLinesCovered = 0;
+            for (const i in sourceFiles) {
+                let filename = sourceFiles[i].filename;
+                let percent = sourceFiles[i]['summary']['lines']['percent'];
+                let count = sourceFiles[i]['summary']['lines']['count'];
+                let covered = sourceFiles[i]['summary']['lines']['covered'];
+                //console.log(i, filename, covered, count, percent);
+                sourceLineCount += sourceFiles[i]['summary']['lines']['count'];
+                sourceLinesCovered += sourceFiles[i]['summary']['lines']['covered'];
+            }
+
+            // Iterate through the source and test files
+            var testAndSourceLineCount = 0;
+            var testAndSourceLinesCovered = 0;
+            for (const i in testAndSourceFiles) {
+                let filename = testAndSourceFiles[i].filename;
+                let percent = testAndSourceFiles[i]['summary']['lines']['percent'];
+                let count = testAndSourceFiles[i]['summary']['lines']['count'];
+                let covered = testAndSourceFiles[i]['summary']['lines']['covered'];
+                //console.log(i, filename, covered, count, percent);
+                testAndSourceLineCount += testAndSourceFiles[i]['summary']['lines']['count'];
+                testAndSourceLinesCovered += testAndSourceFiles[i]['summary']['lines']['covered'];
+            }
+
+            const sourcePercentage = ((sourceLinesCovered / sourceLineCount) * 100);
+            const testAndSourcePercentage = ((testAndSourceLinesCovered / testAndSourceLineCount) * 100);
+            // Average the 2 percentages together (what Xcode does).
+            const avergagePercentage = ((sourcePercentage + testAndSourcePercentage) / 200.0) * 100;
+            console.log('Percentages', sourcePercentage, testAndSourcePercentage);
+            
+            const percentage = avergagePercentage.toFixed(0);
+            console.log(`Coverage Percentage [${avergagePercentage}]`);
             core.setOutput("percentage", percentage);
 
         } catch (error) {
